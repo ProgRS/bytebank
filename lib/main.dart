613d95f92f91e0696cbfdbf171c1a419f385bf0c
ProgRs <lfernandotc@hotmail.com>
2022-10-16 19:43:37 -0300

    Refatoração

--- lib/main.dart
+++ lib/main.dart
@@ -23,52 +23,70 @@ class FormularioTransferencia extends StatelessWidget{
       Widget build(BuildContext context){
          return
            Scaffold(
-               appBar: AppBar(title: Text('Criando Tranferencia'),),
+               appBar: AppBar(title: Text('Criando Tranferencia'),
+               ),
                body: Column(
                  children: <Widget>[
-                   Padding(
-                     padding: const EdgeInsets.all(16.0),
-                     child: TextField(
-                       controller: _controladorCampoNumeroConta,
-                       style: TextStyle(
-                          fontSize: 24.0
-                       ),
-                       decoration: InputDecoration(
-                          labelText: 'Número da conta',
-                          hintText: '0000'
-                       ),
-                       keyboardType: TextInputType.number,
-                     ),
-                   ),Padding(
-                     padding: const EdgeInsets.all(16.0),
-                     child: TextField(
-                       controller: _controladorCampoValor,
-                       style: TextStyle(
-                          fontSize: 24.0
-                       ),
-                       decoration: InputDecoration(
-                          icon: Icon(Icons.monetization_on),
-                          labelText: 'Valor',
-                          hintText: '0.00'
-                       ),
-                       keyboardType: TextInputType.number,
-                     ),
-                   ),
-                ElevatedButton(
+                  Editor(
+                      controlador: _controladorCampoNumeroConta,
+                      dica: '000',
+                      rotulo: 'Numero da conta',
+                      icone: Icons.monetization_on
+                  ),
+                  Editor(dica: '0.00',
+                    controlador: _controladorCampoValor ,
+                     rotulo: 'Valor',
+                     icone: Icons.monetization_on ),
+                RaisedButton(
                     child: Text('Comfirmar'),
                     onPressed: (){
-                      final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
-                      final double? valor =  double.tryParse(_controladorCampoValor.text);
-                      if(numeroConta != null && valor != null) {
-                      final transferenciaCriada =  Transferencia(valor, numeroConta);
-                       debugPrint('$transferenciaCriada');
-                      }
+                      _criaTransferencia();
                     }
                     )
                  ],
                ));
       }
 
+      void _criaTransferencia() {
+         final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+        final double? valor =  double.tryParse(_controladorCampoValor.text);
+        if(numeroConta != null && valor != null) {
+        final transferenciaCriada =  Transferencia(valor, numeroConta);
+         debugPrint('$transferenciaCriada');
+        }
+      }
+
+}
+
+class Editor extends StatelessWidget {
+
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  const Editor({Key? key, required this.controlador, required this.rotulo, required this.dica, required this.icone}) : super(key: key);
+
+  //const Editor({required this.controlador,  required this.rotulo, required this.dica, required this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+            fontSize: 24.0
+        ),
+        decoration: InputDecoration(
+            icon: icone != null ?  Icon(icone) : null,
+            labelText: rotulo,
+            hintText: dica
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
 }
 
 
